d65246ea5010e18b4e7f4ed3f9cd5ef2b95e2a43
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-06-16 16:04:41 -0400

    created server.js, did the npm installs

---
+++ server.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Joker&voldy2019",
+  database: "employeeDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  start();
+});
